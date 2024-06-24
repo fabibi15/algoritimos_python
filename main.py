890ce3d7032555bd7e983460932e09394f83740f
fabiana <fabianadasilvamartins13@gmail.com>
2024-06-24 12:22:46 -0300

    Começo do projeto

---
+++ main.py
@@ -0,0 +1,7 @@
+print('#########################################')
+print('#         ALGORITIMOS EM PYTHON         #')
+print('#########################################')
+print('# Escolha a opção desejada:             #')
+print('#                                       #')
+print('# 1. Fibonacci                          #')
+print('#########################################')

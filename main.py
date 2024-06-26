bc3d5515f7da2e6941c4169c394d785634d4158b
fabiana <fabianadasilvamartins13@gmail.com>
2024-06-26 12:20:30 -0300

    Aula 2

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ print('# Escolha a opção desejada:             #')
 print('#                                       #')
 print('# 1. Fibonacci                          #')
 print('#########################################')
+
+try:
+    choice = int(input())
+
+    if choice == 1:
+        print('FIB')
+    else:
+        print('Opção inválida!')
+except:
+    print('Ocorreu um erro...')
---
+++ scripts/fibonacci.py
@@ -0,0 +1,5 @@
+# Retorna os X primeiros numeros da sequência
+def fibonacci(qtt:int):
+    sequence = [0,1]
+
+    return sequence
